15223164aaf8d1d11f9eb39bb16b006f561636c6
DovgilovaAK <101575183+DovgilovaAK@users.noreply.github.com>
2022-03-15 12:46:13 +0300

    table removed

--- index.py
+++ index.py
@@ -2,9 +2,7 @@ print("Hi")
 
 print("My tel number 375298151627")
 
-for i in range(5):
-    for j in range(9):
-        print(i * j)
+
 
 ch = input("Выберите пункт доставки: ")
 if ch = "самовывоз":
